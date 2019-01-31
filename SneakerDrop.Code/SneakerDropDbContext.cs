42d2370581e425357927cef20f8b68fc48f78914
Henok Tesfaye <henoktesfaye1@gmail.com>
2019-01-30 22:00:33 -0500

    Merge Conflict commit

--- SneakerDrop.Code/SneakerDropDbContext.cs
+++ SneakerDrop.Code/SneakerDropDbContext.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
-using dm = SneakerDrop.Domain.Models;
+using SneakerDrop.Domain.Models;
+
 
 namespace SneakerDrop.Code
 {
     public class SneakerDropDbContext : DbContext
     {
-        public DbSet<dm.User> Users;
+       
 
           //modelbuilder.hasdefaultschema("User")
     }

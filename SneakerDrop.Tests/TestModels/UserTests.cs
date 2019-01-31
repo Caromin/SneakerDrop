3fae52792288fa5d21e6235a654b5fd0a08af3d5
Caromin <carominucf@gmail.com>
2019-01-30 20:49:25 -0500

    outline of models and methods

--- SneakerDrop.Tests/TestModels/UserTests.cs
+++ SneakerDrop.Tests/TestModels/UserTests.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+using SneakerDrop.Domain.Models;
+
 namespace SneakerDrop.Tests.TestModels
 {
     public class UserTests
     {
+
+        public void Test_CheckDbForUser()
+        {
+            var sut = new User
+            {
+                Username = "ian2519",
+                Password = "password1"
+            };
+
+        }
     }
 }

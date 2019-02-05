aa580156d5900f6378e780456bbd62188b756fbe
Nick Vu <nicholas.vu@hotmail.com>
2019-02-04 21:11:10 -0500

    Created methods of AddAddress and GetAddressInfoById for both AddressHelper and AddressTest

---
+++ SneakerDrop.Code/Helpers/AddressHelper.cs
@@ -0,0 +1,28 @@
+﻿using SneakerDrop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SneakerDrop.Code.Helpers
+{
+   public  class AddressHelper
+    {
+        private static SneakerDropDbContext _db = new SneakerDropDbContext();
+
+        public static bool AddAddress(Address address)
+        {
+            
+
+            _db.Addresses.Add(address);
+            return _db.SaveChanges() == 1;
+        }
+        public static Address GetAddressInfoById(Address address)
+        {
+            Address dbAddressInfo = _db.Addresses.Where(a => a.AddressId == address.AddressId).FirstOrDefault();
+
+            return dbAddressInfo;
+        }
+
+    }
+}
--- SneakerDrop.Tests/TestModels/AddressTests.cs
+++ SneakerDrop.Tests/TestModels/AddressTests.cs
@@ -1,14 +1,46 @@
-﻿using System;
+﻿using SneakerDrop.Code.Helpers;
+using SneakerDrop.Domain.Models;
+using System;
 using Xunit;
 
 namespace SneakerDrop.Tests.TestModels
 {
     public class AddressTests
     {
-        [Fact]
-        public void Test_AddAddressByUser()
+        public User testuser = new User
         {
+            UserId = 1,
+            Username = "ian2519",
+            Password = "Password",
+            Firstname = "Ian",
+            Lastname = "Nai",
+            Email = "Email@email.com"
+        };
 
+        public Address address = new Address
+        {
+            Street = "4508 Burnhill Dr",
+            City = "Plano",
+            State = "TX",
+            PostalCode = "75098",
+
+        };
+        //[Fact]
+        //public void Test_AddAddressByUser()
+        //{
+        //    var sut = AddressHelper.AddAddress(address);
+
+        //    Assert.True(sut);
+
+        //}
+        [Fact]
+        public void Test_GetAddressInfoById()
+        {
+            var test = new AddressTests();
+            var value = test.address.AddressId;
+            var sut = AddressHelper.GetAddressInfoById(address);
+
+            Assert.Equal(value, sut.AddressId);
         }
     }
 }

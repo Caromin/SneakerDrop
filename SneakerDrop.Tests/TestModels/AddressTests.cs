8d9333d61268fd95138b052e07c70d5386bd2dab
Nick Vu <nicholas.vu@hotmail.com>
2019-02-04 22:26:51 -0500

    updated addresstest method addaddressbyuser

--- SneakerDrop.Tests/TestModels/AddressTests.cs
+++ SneakerDrop.Tests/TestModels/AddressTests.cs
@@ -7,40 +7,52 @@ namespace SneakerDrop.Tests.TestModels
 {
     public class AddressTests
     {
-        public User testuser = new User
+        public Address sut2;
+        public User user { get; set; }
+
+        public AddressTests()
         {
-            UserId = 1,
-            Username = "ian2519",
-            Password = "Password",
-            Firstname = "Ian",
-            Lastname = "Nai",
-            Email = "Email@email.com"
-        };
+            user = new User()
+            {
+                UserId = 2,
+                Username = "ian2519",
+                Password = "Password",
+                Firstname = "Ian",
+                Lastname = "Nai",
+                Email = "Email@email.com"
+            };
+            sut2 = new Address()
+            {
+                AddressId = 4,
+                Street = "4508 Burnhill Dr",
+                City = "Plano",
+                State = "TX",
+                PostalCode = "75024",
+                User = user
+            };
 
-        public Address address = new Address
+            
+        }
+        [Fact]
+        public void Test_AddAddressByUser()
         {
-            Street = "4508 Burnhill Dr",
-            City = "Plano",
-            State = "TX",
-            PostalCode = "75098",
+            var test = UserHelper.GetUserInfoById(user);
+            var sut = AddressHelper.AddAddress(sut2);
 
-        };
-        //[Fact]
-        //public void Test_AddAddressByUser()
-        //{
-        //    var sut = AddressHelper.AddAddress(address);
 
-        //    Assert.True(sut);
+            Assert.True(sut);
 
-        //}
+        }
         [Fact]
         public void Test_GetAddressInfoById()
         {
             var test = new AddressTests();
-            var value = test.address.AddressId;
-            var sut = AddressHelper.GetAddressInfoById(address);
+            var value = test.sut2.AddressId;
+            var sut = AddressHelper.GetAddressInfoById(sut2);
 
             Assert.Equal(value, sut.AddressId);
         }
+    };
+
     }
-}
+

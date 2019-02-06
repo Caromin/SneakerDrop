92e487309409ed8ca81a78d1a1f02808729e881c
Nick Vu <nicholas.vu@hotmail.com>
2019-02-05 22:35:10 -0500

    updated validator class. Created method LoginValidator in UserViewModel

--- SneakerDrop.Domain/Models/User.cs
+++ SneakerDrop.Domain/Models/User.cs
@@ -30,6 +30,8 @@ namespace SneakerDrop.Domain.Models
         [StringLength(50)]
         [Required]
         public string Password { get; set; }
+
+
     }
 
 }
--- SneakerDrop.Domain/Models/Validator.cs
+++ SneakerDrop.Domain/Models/Validator.cs
@@ -7,6 +7,14 @@ namespace SneakerDrop.Domain.Models
 {
     public class Validator
     {
+        public bool ValidateString(string username, string password, string email)
+        { 
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email))
+            {
+                return true;
+            }
 
+            return false;
+        }
     }
 }

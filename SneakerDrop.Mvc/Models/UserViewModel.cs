ce0d92801c6c551150e8f9ab1dfc8b25851d7fe5
Caromin <carominucf@gmail.com>
2019-02-07 20:41:06 -0500

    added automapper for userviewmodel [#163749204]

--- SneakerDrop.Mvc/Models/UserViewModel.cs
+++ SneakerDrop.Mvc/Models/UserViewModel.cs
@@ -6,6 +6,7 @@ using dm = SneakerDrop.Domain.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using SneakerDrop.Code.Helpers;
+using AutoMapper;
 
 namespace SneakerDrop.Mvc.Models
 {
@@ -33,16 +34,36 @@ namespace SneakerDrop.Mvc.Models
         [Required]
         public string Password { get; set; }
 
-        public void LoginValidator()
+        public void LoginValidator(UserViewModel userView)
         {
+            var loginModel = new ConvertToDomainUser();
+            var userModel = loginModel.MappingUser(userView);
+
             var validator = new dm.Validator();
-            var valCheck =  validator.ValidateString(Username, Password, Email);
-            
+            var valCheck = validator.ValidateString(userModel);
+
             if (valCheck)
             {
-                // waiting for automapper to match models
-                // UserHelper.GetUserInfoById(user);
+                UserHelper.GetUserInfoById(userModel);
             }
+        }
+    }
+
+    public class ConvertToDomainUser : Profile
+    {
+        public static MapperConfiguration userConfig = new MapperConfiguration(cfg => cfg.CreateMap<UserViewModel, dm.User>()
+           .ForMember(d => d.UserId, v => v.MapFrom(src => src.UserId))
+           .ForMember(d => d.Firstname, v => v.MapFrom(src => src.Firstname))
+           .ForMember(d => d.Lastname, v => v.MapFrom(src => src.Lastname))
+           .ForMember(d => d.Username, v => v.MapFrom(src => src.Username))
+           .ForMember(d => d.Email, v => v.MapFrom(src => src.Email))
+           .ForMember(d => d.Password, v => v.MapFrom(src => src.Password)));
+
+        public dm.User MappingUser(UserViewModel userView)
+        {
+            var userMapper = userConfig.CreateMapper();
+
+            return userMapper.Map<UserViewModel, dm.User>(userView);
         }
     }
 }
